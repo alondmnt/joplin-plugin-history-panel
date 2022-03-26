7a01d3d9b1ea78c5d076067d64d62f4253b50309
Alon Diament <alondmnt@users.noreply.github.com>
2022-03-26 15:54:15 +0300

    feature: folding date sections

--- src/panel.ts
+++ src/panel.ts
@@ -1,19 +1,24 @@
 import joplin from 'api';
 
 async function histLinks(histNoteId:string): Promise<string> {
+  const now = new Date();
   let histNote;
   try {
     histNote = await joplin.data.get(['notes', histNoteId], { fields: ['id', 'title', 'body'] });
   } catch {
-    return 'Please set a history note (from the Tools menu) to start logging'
+    return 'Please set a history note (from the Tools menu) to start logging';
   }
 
-  const itemHtml = []
+  const itemHtml = [];
+  let foldTag: string;
+  let dateScope = '';
   for (const line of histNote.body.split('\n')) {
-    const noteTitle = line.match(/\[(.*?)\]/g)[0].slice(1, -1)
-    const noteId = line.match(/\((.*?)\)/g)[0].slice(3, -1)
-    // const noteDate = new Date(line.split(' ')[0])
+    const noteTitle = line.match(/\[(.*?)\]/g)[0].slice(1, -1);
+    const noteId = line.match(/\((.*?)\)/g)[0].slice(3, -1);
+    const noteDate = new Date(line.split(' ')[0]);
+    [foldTag, dateScope] = getFoldTag(now, noteDate, dateScope);
     itemHtml.push(`
+            ${foldTag}
 						<p class="hist-item">
 							<a class="hist-item" href="#" data-slug="${noteId}">
 								${escapeHtml(noteTitle)}
@@ -21,7 +26,32 @@ async function histLinks(histNoteId:string): Promise<string> {
 						</p>
 					`);
   }
+  console.log('html took: ' + (new Date().getTime() - now.getTime()) + ' ms')
   return itemHtml.join('\n');
+}
+
+function getFoldTag(now: Date, noteDate: Date, dateScope: string): [string, string] {
+  /* whenever we pass a threshold, we need to close the previous folding section
+     and start a new one */
+  if ((!dateScope.includes('today')) && (now.getDay() - noteDate.getDay() == 1)) {
+    return ['</details><details><summary>Yesterday</summary>', dateScope + 'today,'];
+  }
+  if ((!dateScope.includes('week')) &&
+      (now.getDay() - noteDate.getDay() > 1) &&
+      (getYearDay(now) - getYearDay(noteDate) <= 7)) {
+    return ['</details><details><summary>Last 7 days</summary>', dateScope + 'week,'];
+  }
+  const strMonth = noteDate.toString().split(' ')[1] + ' ' + noteDate.toString().split(' ')[3]
+  if ((!dateScope.includes(strMonth)) &&
+      (getYearDay(now) - getYearDay(noteDate) > 7)) {
+    return [`</details><details><summary>${strMonth}</summary>`, dateScope + strMonth + ','];
+  }
+
+  return ['', dateScope];
+}
+
+function getYearDay(date: Date): number {
+  return Math.ceil(date.getTime() / 86400000);
 }
 
 // From https://stackoverflow.com/a/6234804/561309
@@ -44,7 +74,10 @@ export default async function updateHistView(panel:string) {
   await joplin.views.panels.setHtml(panel, `
   <div class="container">
     <h3>HISTORY</h3>
+    <details open>
+    <summary>Today</summary>
     ${itemHtml}
+    </details>
   </div>
   `);
 }
