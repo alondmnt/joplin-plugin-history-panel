591bfcdcb4ef4bfb01e5685fa8780cbb08f8b73a
Alon Diament <alondmnt@users.noreply.github.com>
2022-03-30 23:33:07 +0300

    feature: panel title links to the history note

--- src/webview.js
+++ src/webview.js
@@ -1,6 +1,6 @@
 document.addEventListener('click', event => {
 	const element = event.target;
-	if (element.className === 'hist-item') {
+	if ((element.className === 'hist-item') || (element.className === 'hist-title')) {
 		// Post the message and slug info back to the plugin:
 		webviewApi.postMessage({
 			name: 'openHistory',
